2b63fe33898e561deeb82af523fa883032c2dbb7
Daemon2017 <daemon2017@gmail.com>
2022-12-17 21:19:42 +0300

    Добавлено сохранение/чтение обученной модели

--- predict.py
+++ predict.py
@@ -6,8 +6,7 @@ import utils
 combined_df = pd.read_csv('combined_snp_str_map.csv', dtype=str)
 combined_df = utils.get_prepared_df(combined_df)
 
-iterative_imputer = utils.get_fitted_imputer(combined_df)
-joblib.dump(iterative_imputer, 'iterative_imputer.pkl')
+iterative_imputer = joblib.load('iterative_imputer.pkl')
 
 sparse_combined_df = utils.get_sparse_df(combined_df)
 utils.get_imputation_score(iterative_imputer, combined_df, sparse_combined_df)
