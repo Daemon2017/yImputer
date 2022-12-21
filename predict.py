19619bd478bdc6bb42d062e4a9b4ff984fa6bf2d
Daemon2017 <daemon2017@gmail.com>
2022-12-21 20:12:41 +0300

    Добавлена ручка для YFull

--- predict.py
+++ predict.py
@@ -35,5 +35,24 @@ def predict_csv():
     return Response(imputed_df.to_csv(sep=sep, index=False), mimetype='text/csv')
 
 
+@app.route('/predict_yfull', methods=['POST'])
+def predict_yfull():
+    sep = request.args.get('sep')
+    sparse_df = pd.concat([pd.DataFrame(columns=utils.full_yfull_strs_order),
+                           pd.read_csv(request.files['file'], sep=sep, index_col=0, header=None).T])
+    sparse_df = sparse_df.drop(sparse_df.index.to_list()[1:], axis=0)
+    sparse_df = sparse_df[sparse_df.columns.drop(list(sparse_df.filter(regex='_FT|_YS')))]
+    sparse_df = sparse_df[utils.full_yfull_strs_order]
+    for column in sparse_df.columns:
+        sparse_df[column] = sparse_df[column].astype(str).str.split('.').str[0]
+    sparse_df = sparse_df.rename(columns=utils.yfull_to_ftdna_dict)
+    sparse_df = utils.get_prepared_predict_df(sparse_df, False)
+    imputed_df = utils.get_imputed_df(iterative_imputer, sparse_df)
+    ftdna_to_yfull_dict = dict((v, k) for k, v in utils.yfull_to_ftdna_dict.items())
+    imputed_df = imputed_df.rename(columns=ftdna_to_yfull_dict)
+    imputed_df = imputed_df[utils.full_yfull_strs_order]
+    return Response(imputed_df.to_csv(sep=sep, index=False), mimetype='text/csv')
+
+
 if __name__ == '__main__':
     serve(app, host="0.0.0.0", port=8080)
