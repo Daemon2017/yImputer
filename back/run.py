3c4f4d9dce42954f32bf68c9eb39b35db69f4d6c
Daemon2017 <daemon2017@gmail.com>
2024-01-28 23:52:27 +0300

    Добавлено приведение к формату FTDNA

--- back/run.py
+++ back/run.py
@@ -20,7 +20,11 @@ iterative_imputer = joblib.load(os.path.join(os.path.dirname(os.path.realpath(sy
 def predict_yfull():
     sparse_df = utils.get_prepared_predict_df(request)
     imputed_df = utils.get_imputed_df(iterative_imputer, sparse_df[utils.train_strs_order])
-    return Response(imputed_df[utils.full_ftdna_strs_order].to_csv(header=False, sep='\t', index=False),
+    for palindromes in [['DYS385a', 'DYS385b'], ['DYS459a', 'DYS459b'], ['DYS464a', 'DYS464b', 'DYS464c', 'DYS464d'],
+                        ['CDYa', 'CDYb'], ['DYF395S1a', 'DYF395S1b'], ['DYS413a', 'DYS413b'], ['YCAIIa', 'YCAIIb']]:
+        imputed_df[palindromes[0][:-1]] = imputed_df[palindromes].astype(str).apply("-".join, axis=1)
+        imputed_df = imputed_df.drop(columns=palindromes)
+    return Response(imputed_df[utils.ftdna_strs_order].to_csv(header=False, sep='\t', index=False),
                     mimetype=TEXT_PLAIN)
 
 
